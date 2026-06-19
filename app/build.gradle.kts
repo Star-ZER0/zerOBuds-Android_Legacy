430a59a415821f58c16757fa41ed377193f2e441
Star_ZER0 <m@star0.cc>
2026-06-19 18:23:54 +0800

    Do not shrink resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
